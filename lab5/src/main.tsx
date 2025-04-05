f41c6b3475d9cfd6d51c4a1c7675bc9a33d38254
Maxim <144056146+TockePie@users.noreply.github.com>
2025-04-05 23:06:35 +0300

    dark mode

--- lab5/src/main.tsx
+++ lab5/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </NextThemesProvider>
   </StrictMode>
 )
