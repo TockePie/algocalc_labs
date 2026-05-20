17397f6abf21a619ee7f8f198023597838c5d8f3
Maxim <144056146+TockePie@users.noreply.github.com>
2026-05-20 12:28:07 +0300

    normal compiler

--- lab1/vite.config.ts
+++ lab1/vite.config.ts
@@ -1,16 +1,11 @@
-import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
